60602158b9f996145b3aba106f4a81e6d81c7b37
fotolapissable <cipriano.miche@gmail.com>
2026-09-02 12:02:01 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'meds-cache-v1';
+const ASSETS = [
+  './medicinali-giornalieri.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+      .catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request).catch(() => cached))
+  );
+});
+
+// Apre/porta in primo piano l'app quando si tocca una notifica
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientsArr) => {
+      if (clientsArr.length > 0) {
+        clientsArr[0].focus();
+      } else {
+        self.clients.openWindow('./medicinali-giornalieri.html');
+      }
+    })
+  );
+});
